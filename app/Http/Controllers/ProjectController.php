3e7f8ea6d042dd42cdacd29dc9b5164b74b3eff7
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-09-27 10:06:19 +0700

    Create ProjectController

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Project;
+use App\Client;
+use DataTables;
+
+
+class ProjectController extends Controller
+{
+    public function getOnProgressAjax(Request $request)
+    {
+        $projects = Project::with(['client', 'project_type'])->get();
+
+        return DataTables::of($projects)->make(true);
+    }
+
+    public function getOnProgress()
+    {
+        return view('project.list-onprogress');
+    }
+
+    /**
+     * Select client or prospect
+     */
+    public function createStep1()
+    {
+        return view('project.create-select_client');
+    }
+
+    public function createStep1AjaxClient()
+    {
+        $clients = Client::with('type')->where('status', '=', 0)->get();
+
+        return DataTables::of($clients)->make(true);
+    }
+
+    public function createStep1AjaxProspect()
+    {
+        $prospects = Client::with('type')->where('status', '=', 1)->get();
+
+        return DataTables::of($prospects)->make(true);
+    }
+
+    public function createStep2()
+    {
+        return view('project.create-select_type');
+    }
+
+    public function createStep3(Request $request)
+    {
+        $client = Client::find($request->input('client_id'));
+
+        return view('project.create-form', compact('client'));
+    }
+}
