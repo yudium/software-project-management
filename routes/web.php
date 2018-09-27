6f502c8fb088947cdd5cb8a5ebe9aa9c4a5327c5
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-09-27 10:09:22 +0700

    New routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/project/list/onprogress', 'ProjectController@getOnProgress');
+Route::get('/project/list/onprogress/ajax', 'ProjectController@getOnProgressAjax');
+
+Route::get('/project/new/select-client', 'ProjectController@createStep1');
+Route::get('/project/new/select-client/ajax/client', 'ProjectController@createStep1AjaxClient');
+Route::get('/project/new/select-client/ajax/prospect', 'ProjectController@createStep1AjaxProspect');
+
+Route::get('/project/new/select-project-type', 'ProjectController@createStep2');
+Route::get('/project/new/step-3', 'ProjectController@createStep3');
