a7dcdd10b13397692b2472d1d39d9cfed9ac0a27
Maulvi Inayat Ali <maulvi67@gmail.com>
2018-10-10 09:58:28 +0700

    edit header

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -12,21 +12,20 @@
 	<div class="container">
 		<div class="d-flex">
 			<a class="header-brand" href="../index.html">
-				<img src="../demo/brand/tabler.svg" class="header-brand-img" alt="tabler logo">
+				<img src="../demo/logo.svg" class="header-brand-img" alt="tabler logo">
 			</a>
 
 			<div class="d-flex order-lg-2 ml-auto">
-				<div class="nav-item d-none d-md-flex">
-					<a href="https://github.com/tabler/tabler" class="btn btn-sm btn-outline-primary" target="_blank">Source code</a>
-				</div>
+		
 				<div class="dropdown d-none d-md-flex">
 					<a class="nav-link icon" data-toggle="dropdown">
 						<i class="fe fe-bell"></i>
 						<span class="nav-unread"></span>
 					</a>
 					<div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-						<a href="#" class="dropdown-item d-flex">
-							<span class="avatar mr-3 align-self-center" style="background-image: url(demo/faces/male/41.jpg)"></span>
+						{{--  UNTUK FUNCTION NOTIFIKASI  --}}
+						{{--  <a href="#" class="dropdown-item d-flex">
+							<span class="avatar mr-3 align-self-center" style="background-image: url(demo/user.png)"></span>
 							<div>
 								<strong>Nathan</strong> pushed new commit: Fix page load performance issue.
 								<div class="small text-muted">10 minutes ago</div>
@@ -45,21 +44,22 @@
 								<strong>Rose</strong> deployed new version of NodeJS REST Api V3
 								<div class="small text-muted">2 hours ago</div>
 							</div>
-						</a>
+						</a>  --}}
 						<div class="dropdown-divider"></div>
 						<a href="#" class="dropdown-item text-center">Mark all as read</a>
 					</div>
 				</div>
 				<div class="dropdown">
 					<a href="#" class="nav-link pr-0 leading-none" data-toggle="dropdown">
-						<span class="avatar" style="background-image: url(../demo/faces/female/25.jpg)"></span>
+						<span class="avatar" style="background-image: url(../demo/user.png)"></span>
 						<span class="ml-2 d-none d-lg-block">
-							<span class="text-default">Jane Pearson</span>
-							<small class="text-muted d-block mt-1">Administrator</small>
+							<span class="text-default">@if(Auth::check()) {{ Auth::user()->name }} @else Not Loggin @endif</span>
+							<small class="text-muted d-block mt-1">@if(Auth::check()) Administrator @endif</small>
 						</span>
 					</a>
 					<div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-						<a class="dropdown-item" href="#">
+						
+						{{--  <a class="dropdown-item" href="#">
 							<i class="dropdown-icon fe fe-user"></i> Profile
 						</a>
 						<a class="dropdown-item" href="#">
@@ -75,7 +75,7 @@
 						<div class="dropdown-divider"></div>
 						<a class="dropdown-item" href="#">
 							<i class="dropdown-icon fe fe-help-circle"></i> Need help?
-						</a>
+						</a>  --}}
 						<a class="dropdown-item" href="#">
 							<i class="dropdown-icon fe fe-log-out"></i> Sign out
 						</a>
