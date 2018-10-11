4929a658694f4a01b642a6b8853e852287cf770c
Maulvi Inayat Ali <maulvi67@gmail.com>
2018-10-11 22:36:12 +0700

    create middleware to all route

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,82 @@
-@extends('layouts.app')
+<!doctype html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-                <div class="card-body">
-                    @if (session('status'))
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Language" content="en" />
+
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="theme-color" content="#4188c9">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    @include('template.library')
+    <style>
+        .dropdown-item {
+            padding-left: 35px;
+        }
+        .dropdown-item-title {
+            padding-left: 24px;
+            font-weight: bold;
+            background: rgba(216, 216, 216, 0.2);
+        }
+        </style>
+</head>
+
+<body class="">
+
+    <div class="page">
+        <div class="page-single">
+            <div class="container">
+                <div class="row">
+                    <div class="col col-login mx-auto">
+                        <div class="text-center mb-6">
+                            <img src="./demo/brand/tabler.svg" class="h-6" alt="">
+                        </div>
+
+                        @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
+                        @endif
+                        <form method="POST" class="card" action="{{ route('password.email') }}" aria-label="{{ __('Reset Password') }}">
+                            @csrf
+                            <div class="card-body p-6">
+                                <div class="card-title">Forgot password</div>
 
-                    <form method="POST" action="{{ route('password.email') }}" aria-label="{{ __('Reset Password') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
+                                <p class="text-muted">Enter your email address and your password will be reset and
+                                    emailed to you.</p>
+                                <div class="form-group">
+                                    <label class="form-label" for="exampleInputEmail1">Email address</label>
+                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                        name="email" value="{{ old('email') }}" required placeholder="Enter Email">
+                                    @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+                                    @endif
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
+                                </div>
+                                <div class="form-footer">
+                                    <button type="submit" class="btn btn-primary btn-block">Send me new password</button>
+                                </div>
                             </div>
+                        </form>
+                        <div class="text-center text-muted">
+                            Forget it, <a href="./login.html">send me back</a> to the sign in screen.
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,41 +17,44 @@ Route::get('/', function () {
 Route::get('/test',function(){
     return view('template.master');
 });
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/prospect/prospect-list/','ClientController@prospectList')->name('prospectList');
-Route::get('/getProspect','ClientController@getProspect')->name('getProspect');
-Route::get('/prospect/new/prospect-types','ClientController@newProspectType')->name('newProspectTypes');
-Route::get('/prospect/new/prospect','ClientController@getProspectType')->name('getProspectTypegit ');
-Route::get('/prospect/new/prospect-form','ClientController@newProspectForm')->name('newProspectForm');
-Route::post('/prospect/new/prospect-form','ClientController@createProspectForm')->name('createProspectForm');
-Route::get('/prospect/new/prospect-insider','ClientController@newProspectInsider')->name('newProspectInsider');
-Route::post('/prospect/new/prospect-insider','ClientController@createClientInsider')->name('createProspectInsider');
+    Route::get('/prospect/prospect-list/','ClientController@prospectList')->name('prospectList');
+    Route::get('/getProspect','ClientController@getProspect')->name('getProspect');
+    Route::get('/prospect/new/prospect-types','ClientController@newProspectType')->name('newProspectTypes');
+    Route::get('/prospect/new/prospect','ClientController@getProspectType')->name('getProspectTypegit ');
+    Route::get('/prospect/new/prospect-form','ClientController@newProspectForm')->name('newProspectForm');
+    Route::post('/prospect/new/prospect-form','ClientController@createProspectForm')->name('createProspectForm');
+    Route::get('/prospect/new/prospect-insider','ClientController@newProspectInsider')->name('newProspectInsider');
+    Route::post('/prospect/new/prospect-insider','ClientController@createClientInsider')->name('createProspectInsider');
 
-Route::get('/client/client-list','CLientController@clientList')->name('clientList');
-Route::get('/getClient','ClientController@getClient')->name('getClient');
-Route::get('/client/new/client-types','ClientController@newClientType')->name('newClientTypes');
-Route::get('/client/new/client','ClientController@getClientType')->name('getClientTypegit ');
-Route::get('/client/new/client-form','ClientController@newClientForm')->name('newClientForm');
-Route::post('/client/new/client-form','ClientController@createClientForm')->name('createClientForm');
-Route::get('/client/new/client-insider','ClientController@newClientInsider')->name('newClientInsider');
-Route::post('/client/new/client-insider','ClientController@createClientInsider')->name('createClientInsider');
+    Route::get('/client/client-list','CLientController@clientList')->name('clientList');
+    Route::get('/getClient','ClientController@getClient')->name('getClient');
+    Route::get('/client/new/client-types','ClientController@newClientType')->name('newClientTypes');
+    Route::get('/client/new/client','ClientController@getClientType')->name('getClientTypegit ');
+    Route::get('/client/new/client-form','ClientController@newClientForm')->name('newClientForm');
+    Route::post('/client/new/client-form','ClientController@createClientForm')->name('createClientForm');
+    Route::get('/client/new/client-insider','ClientController@newClientInsider')->name('newClientInsider');
+    Route::post('/client/new/client-insider','ClientController@createClientInsider')->name('createClientInsider');
 
-Route::get('/agen/agen-list','AgenController@index')->name('agenList');
+    Route::get('/agen/agen-list','AgenController@index')->name('agenList');
 
-Route::get('/project/list/onprogress', 'ProjectController@getOnProgress')->name('onProgressList');
-Route::get('/project/list/onprogress/ajax', 'ProjectController@getOnProgressAjax');
+    Route::get('/project/list/onprogress', 'ProjectController@getOnProgress')->name('onProgressList');
+    Route::get('/project/list/onprogress/ajax', 'ProjectController@getOnProgressAjax');
 
-Route::get('/project/new/step-1', 'ProjectController@createStep1')->name('newProjectStep1');
-Route::get('/project/new/step-1/ajax/client', 'ProjectController@createStep1AjaxClient')->name('newProjectStep1AjaxClient');
-Route::get('/project/new/step-1/ajax/prospect', 'ProjectController@createStep1AjaxProspect')->name('newProjectStep1AjaxProspect');
+    Route::get('/project/new/step-1', 'ProjectController@createStep1')->name('newProjectStep1');
+    Route::get('/project/new/step-1/ajax/client', 'ProjectController@createStep1AjaxClient')->name('newProjectStep1AjaxClient');
+    Route::get('/project/new/step-1/ajax/prospect', 'ProjectController@createStep1AjaxProspect')->name('newProjectStep1AjaxProspect');
 
-Route::get('/project/new/step-2', 'ProjectController@createStep2')->name('newProjectStep2');
+    Route::get('/project/new/step-2', 'ProjectController@createStep2')->name('newProjectStep2');
 
-Route::get('/project/new/step-3', 'ProjectController@createStep3')->name('newProjectStep3');
-Route::post('/project/new/step-3', 'ProjectController@createStep3Post')->name('newProjectStep3Post');
+    Route::get('/project/new/step-3', 'ProjectController@createStep3')->name('newProjectStep3');
+    Route::post('/project/new/step-3', 'ProjectController@createStep3Post')->name('newProjectStep3Post');
 
-Route::get('/project/new/step-4', 'ProjectController@createStep4')->name('newProjectStep4');
+    Route::get('/project/new/step-4', 'ProjectController@createStep4')->name('newProjectStep4');
+
+    Route::get('/home', 'HomeController@index')->name('home');
+ 
+});
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
