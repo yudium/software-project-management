f8d9284b63ca44cc5968b921b79f200eb6c50df6
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-11-21 11:26:52 +0700

    Add properties to ProspectToClientTransformation

--- app/ProspectToClientTransformation.php
+++ app/ProspectToClientTransformation.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProspectToClientTransformation extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'created_at',
+    ];
+
+    public $timestamps = false;
+
     public function client()
     {
         return $this->belongsTo('App\Client');
