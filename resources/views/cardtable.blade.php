abab36274dd0bf00c10743ff3461b1c96b99f6be
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-11-23 15:53:50 +0700

    Add loader in datatable <table> when processing
    Processing means when datatable is load from ajax

--- resources/views/cardtable.blade.php
+++ resources/views/cardtable.blade.php
@@ -1,8 +1,29 @@
+{{-- This partial view is *ONLY* for datatable <table> --}}
+{{-- NOTE: your datatable <table> should use class '.datatable' --}}
 <div class="card">
-    {{-- <div class="table-responsive"> --}}
-        <!-- removed class: text-nowrap -->
-        <table class="table table-hover table-outline table-vcenter card-table {{ $class ?? '' }}">
-            {{ $slot }}
-        </table>
-    {{-- </div> --}}
+    {{-- you should remove 'active' class after data is has loaded --}}
+    <div class="dimmer active">
+        <div class="loader"></div>
+        <div class="dimmer-content">
+            <table class="table table-hover table-outline table-vcenter card-table {{ $class ?? '' }}">
+                {{ $slot }}
+            </table>
+        </div>
+    </div>
 </div>
+
+<script>
+    require(['datatables', 'jquery', 'moment'], function(datatable, $, moment) {
+        $(document).ready(function(){
+            $('.datatable').on('processing.dt', function (e, settings, processing) {
+                if (processing) {
+                    // start loading
+                    $('.dimmer').addClass('active');
+                } else {
+                    // loading is done
+                    $('.dimmer').removeClass('active');
+                }
+            });
+        });
+    });
+</script>
